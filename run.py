7edcd4d59e4f52f2e27d4a19a52556b1923079ae
BYEONGKI-LAB\user <jbk958>
2018-01-23 14:17:22 +0900

    fix

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
-from LDAscikit.LDAprocess import LDAprocess
-from LDAscikit.Utils import Utils
+from LDAscikit import LDAprocess
+from LDAscikit import Utils
 
-df_matrix = Utils().ReadCSV(filename = "matrix.csv", delimiter = ',', ind_col = 0)
-DocTopic, TopicWord = LDAprocess().LDAexecute(df_matrix, 23,1,20)
-TopicInfo = LDAprocess().TopicInfo(TopicWord)
-#similarity = LDAprocess().TopicDecision(df_matrix, TopicNumFrom = 2, TopicNumTo = 100, CosineOrPerplexity = 1)
-#print(similarity)
+df_matrix = Utils.ReadCSV(filename = "matrix.csv", delimiter = ',', ind_col = 0)
+DocTopic, TopicWord = LDAprocess.LDAexecute(df_matrix, 23,1,20)
+TopicInfo = LDAprocess.TopicInfo(TopicWord)
+similarity = LDAprocess.TopicDecision(df_matrix, TopicNumFrom = 2, TopicNumTo = 100, CosineOrPerplexity = 2)
 
-Utils().WriteXLSX("DocTopic.xlsx", DocTopic)
-Utils().WriteXLSX("TopicWord.xlsx", TopicWord)
-Utils().WriteXLSX("TopicInfo.xlsx", TopicInfo)
+Utils.WriteXLSX("DocTopic.xlsx", DocTopic)
+Utils.WriteXLSX("TopicWord.xlsx", TopicWord)
+Utils.WriteXLSX("TopicInfo.xlsx", TopicInfo)
